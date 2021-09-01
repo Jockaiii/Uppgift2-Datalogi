d6127a683cc851a2977fd95e77f4f85ead30a67b
Karl Sjöstrand <karljsjo@gmail.com>
2021-09-01 16:04:59 +0200

    added method declarations

---
+++ Uppgift2 Datalogi/PathFinder.cs
@@ -0,0 +1,19 @@
+﻿namespace Uppgift2_Datalogi
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    class PathFinder
+    {
+        public int ShortestPath(Node start, Node end)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ShortestPath(Node start, Node visit, Node end)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
