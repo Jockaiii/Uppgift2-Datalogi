5bfa578739f05fec5e282a454b66e770b1b69b12
Karl Sjöstrand <karljsjo@gmail.com>
2021-09-13 19:23:30 +0200

    added xml commentary

--- Uppgift2 Datalogi/Models/NodeCost.cs
+++ Uppgift2 Datalogi/Models/NodeCost.cs
@@ -4,10 +4,21 @@
     using System.Collections.Generic;
     using System.Text;
 
+    /// <summary>
+    /// Stores data related to a node used when using an implementation of Dijkstras algorithm.
+    /// </summary>
     public class NodeCost
     {
         public Node Node { get; set; }
+
+        /// <summary>
+        /// Node to traverse for minimum cost reaching start.
+        /// </summary>
         public NodeCost TowardStart { get; set; }
+
+        /// <summary>
+        /// Cost for reaching start from this node.
+        /// </summary>
         public int CostToStart { get; set; }
 
         public NodeCost(Node node, NodeCost towardStart, int costToStart)
@@ -17,6 +28,10 @@
             CostToStart = costToStart;
         }
 
+        /// <summary>
+        /// String representation of the node cost.
+        /// </summary>
+        /// <returns>Name of node, name of node that leads towards start, and cost for reaching start from this node.</returns>
         public override string ToString()
         {
             return $"{Node.Name}, Start -> {TowardStart.Node.Name}, Cost: {CostToStart}";
